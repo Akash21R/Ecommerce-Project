2a1fe2e9b91b1fb719776a80520157a49e5ba343
Akash R <akash21.rajkumar@gmail.com>
2025-04-25 13:23:38 +0530

    Add files via upload

---
+++ Ecommerce - Project/Assests/Js/index.js
@@ -0,0 +1,9 @@
+const sideNavbar = document.querySelector(".side-navbar");
+// sideNavbar.style.display = "none";
+
+function sidenav(){
+    sideNavbar.style.left = "0";
+}
+function closenav(){
+    sideNavbar.style.left = "-60%";
+}
